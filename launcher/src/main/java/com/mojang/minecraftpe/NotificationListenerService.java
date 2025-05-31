39942e4d0315357296e201da15a7f8181f07cefc
Zonrox <127013771+Zeuroux@users.noreply.github.com>
2025-05-31 23:16:50 +0800

    i will kms

---
+++ launcher/src/main/java/com/mojang/minecraftpe/NotificationListenerService.java
@@ -0,0 +1,8 @@
+package com.mojang.minecraftpe;
+
+/** @noinspection unused*/
+public class NotificationListenerService {
+    public static String getDeviceRegistrationToken() {
+        return "";
+    }
+}
